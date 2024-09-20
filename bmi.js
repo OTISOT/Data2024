8a85b37434af4e6226de7cc6a96628f056995ada
OTISOT <clin8798@gmail.com>
2024-09-20 16:31:05 +0800

    20240920

--- bmi.js
+++ bmi.js
@@ -1,4 +1,12 @@
 var weight = 64;
 var height = 173;
 var bmi = weight/((height/100)**2);
-console.log("hello! Your BMI Value is "+bmi);   
+
+function calcbmi()
+{
+    var weight =document.getElementById("wei").value
+    var height = document.getElementById("hei").value
+    var bmi = weight/((height/100)**2);
+    document.getElementById("bmi").innerHTML=bmi;
+    console.log("hello! Your BMI Value is "+bmi);   
+}
