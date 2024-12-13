d2ddc9e212e1a3664b3e87124c1a63d61d84f19b
OTISOT <clin8798@gmail.com>
2024-12-13 16:59:35 +0800

    20241213

--- seatch_Seqential.js
+++ seatch_Seqential.js
@@ -1,7 +1,7 @@
-Array.prototype.sequentialSearch = function(target) {
+Array.prototype.sequentialSearch = function(field,target) {
     // 'this' refers to the array instance on which the function is called
     for (let i = 0; i < this.length; i++) {
-        if (this[i] === target) {
+        if (this[i][field] === target) {
             return i; // Return the index of the target element
         }
     }
@@ -12,7 +12,7 @@ Array.prototype.sequentialSearch = function(target) {
 const data = [{name:"Amy",Age:8}, {name:"John",Age:18}, {name:"Bob",Age:11}, {name:"Coby",Age:12}, {name:"Dany",Age:11}];
 const target = "Amy";
 
-const index = data.sequentialSearch(target);
+const index = data.sequentialSearch("Age",18);
 if (index !== -1) {
     console.log(`Element found at index: ${index}`);
 } else {
